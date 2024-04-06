f24d51e25b8b1721e092fa3ec41f6ba97af0c267
vasas1d <daniel.vasas1@gmail.com>
2024-04-06 21:15:29 +0200

    fix Player.java

--- main/java/ekke/azft2i/azft2i_chess_twoplayer_20/game/Player.java
+++ main/java/ekke/azft2i/azft2i_chess_twoplayer_20/game/Player.java
@@ -5,40 +5,14 @@ import ekke.azft2i.azft2i_chess_twoplayer_20.pieces.Color;
 public class Player {
 
     private String playerNick;
-    private int playerScore;
     private Color playerColor;
-
-
-
-
-
     public Player(String name, Color color) {
         this.playerNick = name;
         this.playerColor = color;
-        this.playerScore = 0;
     }
     public String getName() {
         return playerNick;
     }
 
-    public void setName(String name) {
-        this.playerNick = name;
-    }
-
-    public int getScore() {
-        return playerScore;
-    }
-
-    public void setScore(int score) {
-        this.playerScore = score;
-    }
-
-    public void increaseScore(int points) {
-        this.playerScore += points;
-    }
-
-    public Color getPlayerColor() {
-        return playerColor;
-    }
 
 }
